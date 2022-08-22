adff8680a0f507669008605cf75824f3b043c856
Erik Rospo <rerik.work@gmail.com>
2022-08-22 13:41:50 -0700

    more changes to the options, as well the pbars

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import os
 import numpy as np
 import scipy.ndimage as nd
 import PIL.Image
-from IPython.display import clear_output, Image, display
 from google.protobuf import text_format
-from tqdm import trange
+import json
 import caffe
 import cv2
 
+with open("settings.json","rt") as f:
+    settings=json.load(f)
 # If your GPU supports CUDA and Caffe was built with CUDA support,
 # uncomment the following to run Caffe operations on the GPU.
 # caffe.set_mode_gpu()
@@ -18,8 +19,15 @@ model_path = './models/' # substitute your path here
 net_fn   = model_path + 'deploy.prototxt'
 param_fn = model_path + 'bvlc_googlenet.caffemodel'
 
+# setting up progress bar.
+status={
+    "topLevel":"",
+    "octave":"",
+    "innerLevel":""
+}
 # Patching model to be able to compute gradients.
 # Note that you can also manually add "force_backward: true" line to "deploy.prototxt".
+
 model = caffe.io.caffe_pb2.NetParameter()
 text_format.Merge(open(net_fn).read(), model)
 model.force_backward = True
@@ -38,7 +46,6 @@ def showarray(a, fmt='jpeg'):
     a = np.uint8(np.clip(a, 0, 255))
     f = StringIO()
     PIL.Image.fromarray(a).save(f, fmt)
-    display(Image(data=f.getvalue()))
 def objective_L2(dst):
     dst.diff[:] = dst.data 
 
@@ -64,6 +71,8 @@ def make_step(net, step_size=1.5, end='inception_4c/output',
     if clip:
         bias = net.transformer.mean['data']
         src.data[:] = np.clip(src.data, -bias, 255-bias)
+def fancy_status():
+    print("Outer Level: "+status["topLevel"]+" Octave: "+status["octave"]+" Inner Level: "+status["innerLevel"])
 def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, 
               end='inception_4c/output', clip=True, **step_params):
     # prepare base images for all octaves
@@ -74,6 +83,9 @@ def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4,
     src = net.blobs['data']
     detail = np.zeros_like(octaves[-1]) # allocate image for network-produced details
     for octave, octave_base in enumerate(octaves[::-1]):
+        status["innerLevel"]="(?/?)"
+        status["octave"]="({}/{})".format(octave+1,octave_n)
+        fancy_status()
         h, w = octave_base.shape[-2:]
         if octave > 0:
             # upscale details from the previous octave
@@ -83,32 +95,38 @@ def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4,
         src.reshape(1,3,h,w) # resize the network's input image size
         src.data[0] = octave_base+detail
         for i in range(iter_n):
+            status["innerLevel"]="({}/{})".format(i+1,iter_n)
+            fancy_status()
             make_step(net, end=end, clip=clip, **step_params)
-            
-            # visualization
-            # vis = deprocess(net, src.data[0])
-            # if not clip: # adjust image contrast if clipping is disabled
-            #     vis = vis*(255.0/np.percentile(vis, 99.98))
-            # clear_output(wait=True)
+
         # extract details produced on the current octave
         detail = src.data[0]-octave_base
     # returning the resulting image
     return deprocess(net, src.data[0])
 
 os.system('clear')    
-img = np.float32(PIL.Image.open('sky1024px.jpg'))
-os.makedirs('frames',exist_ok=True)
+img = np.float32(PIL.Image.open(settings['dream']['inputPath']))
+os.makedirs(os.path.split(settings["dream"]["output"]["images"]["path"])[0],exist_ok=True)
 frame = img
 frame_i = 0
 h, w = frame.shape[:2]
 s = 0.05 # scale coefficient
 paths=[]
-for i in trange(1000):
-    frame = deepdream(net, frame)
-    PIL.Image.fromarray(np.uint8(frame)).save("frames/%04d.jpg"%frame_i)
-    paths.append("frames/%04d.jpg"%frame_i)
-    frame = nd.affine_transform(frame, [1-s,1-s,1], [h*s/2,w*s/2,0], order=1)
-    # frame=nd.rotate(frame,0.2)
+for i in range(int(settings['dream']['iterations'])):
+    status["topLevel"]="({}/{})".format(i+1,settings['dream']['iterations'])
+    status["octave"]="(?/?)"
+    status["innerLevel"]="(?/?)"
+    fancy_status()
+    frame = deepdream(net, frame,iter_n=int(settings['dream']['iterationsPer']),octave_n=int(settings['dream']['octaves']),octave_scale=float(settings['dream']['octaveScale']))
+    fp=settings["dream"]["output"]["images"]["path"]%frame_i
+    PIL.Image.fromarray(np.uint8(frame)).save(fp)
+    paths.append(fp)
+    frame = nd.affine_transform(frame,
+                               [float(settings["transform"]["affine"]["x_factor"]),
+                                float(settings["transform"]["affine"]["y_factor"]),1],
+                           [h*s*float(settings["transform"]["affine"]["y_center"]),
+                            w*s*float(settings["transform"]["affine"]["x_center"]),0], order=1)
+    frame=nd.rotate(frame,float(settings["transform"]["rotate"]["angle"]))
     frame_i += 1
     
 
@@ -119,7 +137,7 @@ for filename in paths:
         
 print("done reading")
 print("writing video")
-out = cv2.VideoWriter("res2.mp4",cv2.VideoWriter_fourcc(*'mp4v'), 15, [1024,575])
+out = cv2.VideoWriter(settings['dream']["output"]["video"]['path'],cv2.VideoWriter_fourcc(*settings['dream']["output"]["video"]['codec']),float(settings['dream']["output"]["video"]['fps']), [h,w])
 for i in range(len(imgArray)):
     out.write(imgArray[i])
 out.release()
